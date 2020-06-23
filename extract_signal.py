5077eec4429a877e0fe2ef880fa08c9d2f6bb355
Ry4an Brase <ry4an-githubpublic@ry4an.org>
2020-06-23 11:30:14 -0400

    Argument parsing (and formatted)

--- extract_signal.py
+++ extract_signal.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 import sys
 import json
+import argparse
 
 from bs4 import BeautifulSoup
 
@@ -10,32 +11,44 @@ from bs4 import BeautifulSoup
 def parse(filehandle):
     # takes unparsed html from http://192.168.100.1/cmSignalData.htm
     # returns dict[title:string][channel:int][value:string] => float
-    soup = BeautifulSoup(filehandle, 'html.parser')
+    soup = BeautifulSoup(filehandle, "html.parser")
 
     measurements = defaultdict(lambda: defaultdict(dict))
 
-    tables = [c.table for c in soup.html.body.find_all('center')]
+    tables = [c.table for c in soup.html.body.find_all("center")]
 
     for table in tables:
-        (header, id_row, *rows) = table.tbody.find_all('tr', recursive=False)
+        (header, id_row, *rows) = table.tbody.find_all("tr", recursive=False)
         title = header.th.get_text()
-        ids = [ int(cell.get_text()) for cell in id_row.find_all('td')[1:] ]
+        ids = [int(cell.get_text()) for cell in id_row.find_all("td")[1:]]
         for row in rows:
-            for cell in row.find_all('td', recursive=False):
+            for cell in row.find_all("td", recursive=False):
                 if cell.table:
-                    cell.table.extract() # remove stupid refresh note
+                    cell.table.extract()  # remove stupid refresh note
 
-            (field, *values) = [ cell.get_text().strip() for cell in row.find_all('td') ]
+            (field, *values) = [cell.get_text().strip() for cell in row.find_all("td")]
             for (the_id, value) in zip(ids, values):
                 measurements[title][the_id][field] = value
     return measurements
 
 
-# main
-if len(sys.argv) == 1:
-    print("Usage: {} FILE1 [FILE2 ...]".format(sys.argv[0]))
+def main():
+    parser = argparse.ArgumentParser(
+        description="Parse and emit surfboard signal status."
+    )
+    parser.add_argument(
+        "files",
+        metavar="FILE",
+        type=argparse.FileType('r'),
+        nargs="+",
+        help="saved HTTP responses to process",
+    )
 
-for filename in sys.argv[1:]:
-    with open(filename) as ff:
-        values = parse(ff)
+    args = parser.parse_args()
+    for fh in args.files:
+        values = parse(fh)
         print(json.dumps(values, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
